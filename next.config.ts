f35a91c5b42635492490abddbe930fa670c0f19b
jyotiprakash-m <jyotiprakash@settlemintindia.com>
2025-01-29 12:25:36 +0530

    fix: enable dockerize

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  poweredByHeader: false,
 };
 
 export default nextConfig;
